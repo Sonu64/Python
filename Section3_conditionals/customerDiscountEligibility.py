63d2b6dce5b71d5f7f96336f6bb0b4ce6ec0301d
Sonu64 <sonusantu64@gmail.com>
2025-08-03 07:04:27 +0530

    Create customerDiscountEligibility.py

---
+++ Section3_conditionals/customerDiscountEligibility.py
@@ -0,0 +1,9 @@
+customer_name = "Amit"
+customer_age = 90
+total_purchase = 1200
+
+if total_purchase > 1000:
+    print(f"Eligible for discount")
+
+if customer_age >= 60:
+    print(f"Senior Citizen")
