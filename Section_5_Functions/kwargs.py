081b3daf5afde2676074bebd389d28dd11fe06b6
Sonu64 <sonusantu64@gmail.com>
2025-08-06 13:40:56 +0530

    Create kwargs.py

---
+++ Section_5_Functions/kwargs.py
@@ -0,0 +1,5 @@
+def specialChai(*ingredients, **extras):
+    print(f"Ingredients: {ingredients}")
+    print(f"Extras: {extras}")
+
+specialChai("Ginger", "Kesar", "Elaichi", foam="No Foam", sweetner="Sugar")
