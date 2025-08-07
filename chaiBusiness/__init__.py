5f7c09fca418f50f58afd152e74bdd9f119ebea9
Sonu64 <sonusantu64@gmail.com>
2025-08-07 09:04:01 +0530

    created __init__ folder and some other import methods

---
+++ chaiBusiness/__init__.py
@@ -0,0 +1,2 @@
+# Turns a Folder into a Python Package/Module
+# We don't need this above Python 3.3
--- chaiBusiness/main.py
+++ chaiBusiness/main.py
@@ -2,6 +2,20 @@
 
 # print(f"Printing flavor: {recipes.flavors.masalaChai()}")
 
+# Avoid importing everything from a package 
+# from recipes import *
+# from utils import *
+
+"""
+The core difference between 'import moduleName' and 'from moduleName import *'is what gets added 
+to your current namespace.
+
+import math imports the entire math module as a single object named math. 
+You must prefix all module functions with math.(e.g., math.sqrt(25)).
+from math import * imports all public names (functions, variables, etc.) from the math module 
+directly into your current namespace. You can then use them without a prefix (e.g., sqrt(25)).
+"""
+
 from recipes.flavors import masalaChai, gingerChai
 print(f"Printing flavor: {masalaChai()}")
 print(f"Printing new flavor: {gingerChai()}")
