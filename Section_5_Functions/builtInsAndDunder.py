fdf1929df98b06baf24a6b427cad991a5e47a004
Sonu64 <sonusantu64@gmail.com>
2025-08-07 08:37:12 +0530

    Create builtInsAndDunder.py

---
+++ Section_5_Functions/builtInsAndDunder.py
@@ -0,0 +1,8 @@
+def chaiFlavor(flavor="Masala"):
+    """A Function to return the flavor of chai""" # Must be the 1st Line of function
+    return flavor
+
+print(f"Function Documentation: {chaiFlavor.__doc__}")
+print(f"Function Name: {chaiFlavor.__name__}")
+
+help(len)
