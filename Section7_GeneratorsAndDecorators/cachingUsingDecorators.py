54151b768c216f8b9f90b7733761792c1365205b
Sonu64 <sonusantu64@gmail.com>
2025-08-11 11:44:15 +0530

    Update cachingUsingDecorators.py

--- Section7_GeneratorsAndDecorators/cachingUsingDecorators.py
+++ Section7_GeneratorsAndDecorators/cachingUsingDecorators.py
@@ -4,11 +4,13 @@ from functools import wraps
 
 def cache_results(func):
     # Write your code below this line
+    lastResult = dict()
     @wraps(func)
     def wrapperFunction(*args, **kwargs):
-        lastResult = dict()
-        result = int(args[0]) * int(args[1])
+        
+        result = func(*args, **kwargs)
 
+        # If not calling for 1st time
         if len(lastResult) != 0:
             if (args[0]==lastResult['firstArgument'] and args[1]==lastResult['secondArgument']):
                 return (f"From Cache: {lastResult['result']}")
@@ -17,6 +19,7 @@ def cache_results(func):
                 lastResult['secondArgument'] = args[1]
                 lastResult['result'] = result
                 return (f"Computed: {result}")
+        # If calling for 1st time
         else:
             lastResult['firstArgument'] = args[0]
             lastResult['secondArgument'] = args[1]
