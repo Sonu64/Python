4f46e8a8e81966834c15d81f568f50011bc68565
Sonu64 <sonusantu64@gmail.com>
2025-08-15 08:30:37 +0530

    Update chaiComposition.py

--- Section8_OOP/chaiComposition.py
+++ Section8_OOP/chaiComposition.py
@@ -15,9 +15,11 @@ class ChaiShop:
     # Not yet creating object like BaseChai("Regular"), just keeping an instance
     chai_cls = BaseChai
 
+    # self.chai is becoming an object of BaseChai
     def __init__(self):
         self.chai = self.chai_cls("Regular")
 
+    # As self.chai is object of BaseChai, it will have the property of 'type'
     def serve(self):
         print(f"Serving {self.chai.type} chai in the shop")
         self.chai.prepare()
