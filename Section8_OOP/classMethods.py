513ec4a6f04cc7916f5719d18441eb7023bc922f
Sonu64 <sonusantu64@gmail.com>
2025-08-16 09:27:58 +0530

    Update classMethods.py

--- Section8_OOP/classMethods.py
+++ Section8_OOP/classMethods.py
@@ -8,18 +8,32 @@ class ChaiOrder:
     # which under-the-hood calls the original constructor
     @classmethod
     def fromDict(cls, orderData):
-        orderDict = {
-            "teaType":orderData['teaType'],
-            "sweetness":orderData['sweetness'],
-            "size":orderData['size']
-        }
         return cls(
-            orderDict['teaType'],
-            orderDict['sweetness'],
-            orderDict['size']
+            orderData['teaType'],
+            orderData['sweetness'],
+            orderData['size']
         )
     
     @classmethod
     def fromString(cls, orderString):
         teaType, sweetness, size = orderString.split(",")
         return cls(teaType, sweetness, size)
+    
+    def describeOrder(self):
+        return f"{self.teaType} Tea with {self.sweetness} sweetness and cup size is {self.size}."
+    
+
+order1 = ChaiOrder.fromDict({"teaType":"Masala","sweetness":"Medium", "size":"Large"})
+order2 = ChaiOrder.fromString("Ginger, Low, Small")
+
+print(order1.describeOrder())
+print(order2.describeOrder())
+
+# Using available dunders on the objects
+
+#__dict__ to show the passed properties in dictionary format
+print(f"Order 1 Properties: {order1.__dict__}")
+print(f"Order 2 Properties: {order2.__dict__}")
+
+
+
