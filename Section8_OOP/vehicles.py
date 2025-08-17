84aecbc0bdb575849bcfbfeeb17df8e46a8473d3
Sonu64 <sonusantu64@gmail.com>
2025-08-17 08:20:40 +0530

    Update vehicles.py

--- Section8_OOP/vehicles.py
+++ Section8_OOP/vehicles.py
@@ -2,7 +2,7 @@ class Engine:
     def __init__(self, horsepower):
         self.horsepower = horsepower
     
-    def get_engine_info():
+    def get_engine_info(self):
         return f"{self.horsepower} HP Engine"
 
 class Vehicle:
@@ -11,10 +11,11 @@ class Vehicle:
         self.brand = brand
         self.model = model
         self.engine = Engine(150)
-        total_vehicles += 1
+        self._rental_price = 0
+        Vehicle.total_vehicles += 1
     
     def get_details(self):
-        return [self.brand, self.model, self.get_engine_info()]
+        return f"{self.brand}, {self.model}, {self.engine.get_engine_info()}"
         
     @staticmethod
     def get_vehicle_type():
@@ -24,5 +25,18 @@ class Vehicle:
     def get_total_vehicles(cls):
         return cls.total_vehicles
     
+    @property
+    def rental_price(self):
+        return self._rental_price
     
+    @rental_price.setter
+    def rental_price(self, rp):
+        if rp > 0:
+            self._rental_price = rp
     
+class Car(Vehicle):
+    def __init__(self, brand, model, engine, seats):
+        super().__init__(brand, model, engine)
+        self.seats = seats
+    def get_details(self):
+        return f"{super().get_details()} - Seats: {self.seats}"
