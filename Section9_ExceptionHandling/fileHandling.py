c2978a12577422ec3c85fe1bf1df64aafe08b246
Sonu64 <sonusantu64@gmail.com>
2025-08-19 08:01:04 +0530

    file handling with 'with' keyword

--- Section9_ExceptionHandling/fileHandling.py
+++ Section9_ExceptionHandling/fileHandling.py
@@ -1,9 +1,15 @@
 # Older way of handling files in Python
 try:
-    data = open("datas.txt", "w")
+    data = open("data.txt", "w")
+    print("File Opened, writing to file...")
     data.write("File Loaded to Memory, Written, then Unloaded back !")
 except Exception as e:
     print(e)
 finally:
     data.close()
-
+    print("File Closed")
+# Newer way using with keyword. Handles opening and closing on its own
+with open("data.txt", "a+") as file:
+    print("\nOpening File again for Appending...")
+    file.write("Hello, world !")
+    print("File Closed")
