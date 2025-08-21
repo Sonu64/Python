9794b56f3cfbce2ebe53537d04733cade6b55cda
Sonu64 <sonusantu64@gmail.com>
2025-08-21 09:18:58 +0530

    Update selfIntroGenerator.py

--- Utilities/selfIntroGenerator.py
+++ Utilities/selfIntroGenerator.py
@@ -2,9 +2,9 @@ from datetime import date
 # Getting the information
 name = input("Enter your name: ")
 age = int(input("Enter your age: "))
-city = input("Enter your city: ")
-proffession = input("Enter your Proffession: ")
-hobby = input("Enter your hobby: ")
+city = input("Enter your city: ").strip()
+proffession = input("Enter your Proffession: ").strip()
+hobby = input("Enter your hobby: ").strip()
 
 output = f"""Hello, my name is {name}. I am {age} years old and live in {city}.
 I work as a {proffession} and I absolutely enjoy {hobby} during my free time."""
