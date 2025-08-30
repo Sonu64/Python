1ba82c7972833cb4e429425b510308a42df2c3a6
Sonu64 <sonusantu64@gmail.com>
2025-08-30 10:46:03 +0530

    Create timer.py

---
+++ Utilities/timer.py
@@ -0,0 +1,28 @@
+# The character '\r' (carriage return) moves the cursor to 
+# the beginning of the current line without advancing to the next line. This allows 
+# subsequent output to overwrite the text that was already on that line
+
+
+import time
+
+while True:
+    try:
+        seconds = int(input("Enter the number of seconds: "))
+        if (seconds < 0):
+            print("Please enter a Positive number !")
+            continue
+    except ValueError as e:
+        print("Please enter a number !")
+        continue
+    break
+
+
+
+for remaining in range(seconds, 0, -1):
+    mins = remaining // 60
+    secs = remaining % 60
+    timeFormat = f"{mins:02}:{secs:02}"
+    print(f"Time Left: {timeFormat}",end="\r")
+    time.sleep(1)
+
+print("\nTime's Up !")
